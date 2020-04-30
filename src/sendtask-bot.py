4ae725803c52bf5e2f562570b9a5fb8b3c2ae30f
Nikolay Suvorov <nsuvorov83@gmail.com>
2020-05-01 00:10:03 +0300

    Subject was shorten toward 40 symbols plus three dots

--- src/sendtask-bot.py
+++ src/sendtask-bot.py
@@ -1,11 +1,12 @@
 import os
 import telebot
 import smtplib
+import textwrap
 from email.mime.text import MIMEText
 
 #Configuration
 CFG_TOKEN = os.environ.get('CFG_TOKEN') #TELEGRAM_BOT_TOKEN
-CFG_OWNER_ID = os.environ.get('CFG_OWNER_ID') #YOUR_USER_ID_IN_TELEGRAM
+CFG_OWNER_ID = int(os.environ.get('CFG_OWNER_ID')) #YOUR_USER_ID_IN_TELEGRAM
 CFG_SMTP_LOGIN = os.environ.get('CFG_SMTP_LOGIN') #'%YOUR_SMTP_LOGIN_ON_YANDEX%'
 CFG_SMTP_PASS = os.environ.get('CFG_SMTP_PASS') #'%YOUR_SMTP_PASS_ON_YANDEX%'
 CFG_SMTP_FROM = os.environ.get('CFG_SMTP_FROM') #'%FROM_EMAIL_ADDRESS%'
@@ -23,11 +24,20 @@ def get_text_messages(message):
 
 def do_next(message):
     #Sening a message to outlook
-    #Message mime creation
-    msg = MIMEText("Задача из Telegram")
-    msg['Subject'] = f'[TASK] {message.text}'
+    
+    #Check if Subject is too long
+    txt = message.text
+    msg = MIMEText('')
+    if len(txt) > 40:
+        txt = textwrap.shorten(txt, width=40, placeholder="...")
+        msg = MIMEText(message.text)
+    else:
+        msg = MIMEText("Задача из Telegram")
+    
     msg['From'] = CFG_SMTP_FROM
     msg['To'] = CFG_SMTP_TO
+    msg['Subject'] = f'[TASK] {txt}'
+
     try:
         server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
         server.login(CFG_SMTP_LOGIN, CFG_SMTP_PASS)
